7b37fb421e7b83176b8fc97dfc2b4fac16ba353b
Matic Isovski <mi6568@student.uni-lj.si>
2020-07-25 10:55:37 +0200

    loading&showing data

---
+++ NeuralNetworks/intro.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# normalization
+train_images = train_images / 255
+test_images = test_images / 255
+
+plt.imshow(train_images[8])
+plt.show()
