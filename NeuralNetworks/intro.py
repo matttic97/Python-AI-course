bc7640725777df656df0e91da049eaa175072564
Matic Isovski <mi6568@student.uni-lj.si>
2020-07-25 11:25:28 +0200

    building & evaluating model

--- NeuralNetworks/intro.py
+++ NeuralNetworks/intro.py
@@ -14,5 +14,18 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images / 255
 test_images = test_images / 255
 
-plt.imshow(train_images[8])
-plt.show()
+'''plt.imshow(train_images[8])
+plt.show()'''
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28, 28)),
+    keras.layers.Dense(128, activation = 'relu'),
+    keras.layers.Dense(10, activation = 'softmax')
+])
+
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(train_images, train_labels, epochs = 10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Tested acc:', test_acc)
